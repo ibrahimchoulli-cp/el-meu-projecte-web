a290245f91313902a5a86ecaa6ecdee1a01055b9
ibrah <ibrahim.choulli@asatorras.cat>
2026-03-17 10:43:33 +0100

    Commit inicial - estructura bàsica

---
+++ script.js
@@ -0,0 +1,7 @@
+// Agafar el botó
+const button = document.getElementById('miButton');
+
+// Afegir funcionalitat al botó
+button.addEventListener('click', function() {
+    alert('Has fet clic al botó! 🎉');
+});
